0d32594613852dff070922b97e5488bea209f474
Shreya <50589991+1302shreya@users.noreply.github.com>
2019-05-13 22:14:10 +0530

    Add files via upload

---
+++ Assignment1A.py
@@ -0,0 +1,43 @@
+import re
+import logging
+import datetime
+
+def validate(password):
+	flag=0
+	reasons=""
+	if re.findall('[a-z]',password)==[]:  #checking condition for presence of lowercase letter
+		flag=1
+		reasons+=" Lower case letter absent."
+	if re.findall('[A-Z]',password)==[]:  #checking condition for  presence of uppercase letter
+		flag=1
+		reasons+=" Upper case letter absent."
+	if re.findall('[0-9]',password)==[]:  #checking condition for presence of a digit
+		flag=1
+		reasons+=" Digit absent."
+	if re.findall('[$#@]',password)==[]:  #checking condition for presence of a special character (@,# or $)
+		flag=1
+		reasons+=" Special character(#,@ or $) absent"
+	if len(password)<5: #checking if length is under the limit
+		flag=1
+		reasons+=" Length under the limit."
+	if len(password)>13: #checking if length over the limit
+		flag=1
+		reasons+=" Length over the limit."
+	if flag==0:
+		print("Valid ")
+		return "Valid"
+	else:
+		mystring="Not Valid because: "+reasons
+		print("Not Valid ")
+		return mystring
+		
+
+if __name__=="__main__":
+	password=input("Enter your password: ") #Compiled using python3, so used input instead of raw_input
+	output=validate(password)
+	#print(output)
+
+	logging.basicConfig(filename='example1.log',level=logging.DEBUG)
+	string1="Input: "+password+","+"Output: "+output
+	logging.info(string1)
+
